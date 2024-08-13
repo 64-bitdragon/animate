357c45b448a871d8a7fee771ada2424dcb989326
Mark Tanner <mark.tanner@argusmedia.com>
2024-08-13 09:32:07 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import MainWindow from "./lib/MainWindow.svelte";
+</script>
+
+<main>
+  <MainWindow />
+</main>
+
+<style lang="scss">
+  main {
+    width: 100%;
+    height: 100%;
+  }
+</style>
